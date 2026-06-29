8c7d58da4720cb469ab88fea4572728d18304555
KhalfanK <khalfan.k@outlook.com>
2026-06-29 22:51:44 +0100

    refactor: purge old files from main branch

--- infra/modules/iam/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "gatus_exec_role_arn" {
-  description = "the ARN of the ECS task execution role for gatus to pull from ECR"
-  value       = aws_iam_role.gatus.arn
-}
-
-output "gatus_task_role_arn" {
-  description = "the ARN of the ECS task role for gatus to access SES"
-  value       = aws_iam_role.gatus_task_role.arn
-}
--- infra/modules/security/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "alb_sg_id" {
-  description = "security group for alb"
-  value = aws_security_group.alb_sg.id
-}
-
-output "gatus_task_sg_id" {
-  description = "security group for alb"
-  value = aws_security_group.gatus_task_sg.id
-}
